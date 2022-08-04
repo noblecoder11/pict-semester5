74ef738e8bbb7417e2dcc19f3e5769d09a68ede4
Shreyash Halge <shreyash.shalge@gmail.com>
2022-08-04 17:41:04 +0530

    Create a3.sql

---
+++ dbms/a3.sql
@@ -0,0 +1,4 @@
+select * from employee natural join project natural join dept where pyear=2022;
+select * from employee natural join project where plocation!='Pune';
+select fname, designation, dlocation, join_date from employee natural join dept;
+select * from employee natural join dept;
